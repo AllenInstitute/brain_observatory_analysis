0addce755519efa984f44d5715f5a5c0a11bcde4
Matt Davis <mattjdavis@gmail.com>
2023-02-08 11:09:06 -0800

    added change_detection_module for mindscope_qc

---
+++ brain_observatory_analysis/behavior/change_detection/data_wrangling/change_detection_dataset.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+from . import utils
+from . import pkl_translator
+from . import annotate
+from . import extended_trials_dataframe as ext_trials_df
+
+import mindscope_qc.data_access.from_lims as from_lims
+
+
+class ChangeDetectionDataset(object):
+    def __init__(self, data_file=None, behavior_session_id=None,
+                 catch_hack: bool = False):
+        """Class for loading and manipulating change detection data,
+        using legacy "foraging" style data structures
+
+        Parameters
+        ----------
+        data_file : str, optional
+            path to data file, by default None
+        behavior_session_id : int, optional
+            behavior session id, by default None
+        catch_hack : bool, optional
+            whether to use the catch hack, by default False
+        """
+
+        self.behavior_session_id = behavior_session_id
+        self.catch_hack = catch_hack
+        self.raw_data = None
+        self.core_data = None
+        self.extended_trials_df = None
+
+        if data_file:
+            self.data_file = data_file
+        elif behavior_session_id:
+            func = from_lims.get_stimulus_pkl_filepath_for_behavior_session
+            self.data_file = func(behavior_session_id)
+
+        self._read_raw_data()
+        self._annotate_licks()
+        self._create_extended_dataframe()
+
+
+    def _read_raw_data(self):
+        self.raw_data = pd.read_pickle(self.data_file)
+        if self.catch_hack:
+            self.core_data = utils.catch_trials_pkl_hack(self.raw_data)
+        self.core_data = pkl_translator.data_to_change_detection_core(self.raw_data)
+
+
+    def _annotate_licks(self):
+        self.core_data["licks"] = annotate.annotate_licks(self.core_data)
+
+
+    def _create_extended_dataframe(self):
+        self.extended_trials_df = ext_trials_df.create_extended_dataframe(self.core_data)
+
+
+    # GETTER METHODS
+    def get_clean_data(self):
+        return self.clean_data
+
+    # get core data
+    def get_core_data(self):
+        return self.core_data
+
+    # get raw data
+    def get_raw_data(self):
+        return self.raw_data
+
+    # get data file
+    def get_data_file(self):
+        return self.data_file
+
+    # get extended trials dataframe
+    def get_extended_trials_df(self):
+        return self.extended_trials_df
---
+++ brain_observatory_analysis/behavior/change_detection/data_wrangling/extended_trials_dataframe.py
@@ -0,0 +1,107 @@
+
+import pandas as pd
+import datetime
+import json
+import numpy as np
+import logging
+
+from . import annotate
+
+logger = logging.getLogger(__name__)
+
+def create_extended_dataframe(core_data: dict,
+                              visual_stimuli: bool = None,
+                              *args,
+                              **kwargs) -> pd.DataFrame:
+    """ creates a trials dataframe from a detection-of-change session
+
+    Parameters
+    ----------
+    filename : str
+        file path of a pickled detection-of-change session
+
+    Returns
+    -------
+    trials : pandas DataFrame
+    """
+    # data = pd.read_pickle(filename)
+
+    trials = core_data['trials']
+    metadata = core_data['metadata']
+    licks = core_data['licks']
+    time = core_data['time']
+
+    trials = trials[~pd.isnull(trials['reward_times'])].reset_index(drop=True)
+
+    # add some columns to the dataframe
+    # column : key
+    keydict = {
+        'mouse_id': 'mouseid',
+        'response_window': 'response_window',
+        'task': 'task',
+        'stage': 'stage',
+        'session_duration': 'stoptime',
+        'user_id': 'userid',
+        'LDT_mode': 'lick_detect_training_mode',
+        'blank_screen_timeout': 'blankscreen_on_timeout',
+        'stim_duration': 'stim_duration',
+        'blank_duration_range': 'blank_duration_range',
+        'prechange_minimum': 'delta_minimum',
+        'stimulus_distribution': 'stimulus_distribution',
+        'stimulus': 'stimulus',
+        'distribution_mean': 'delta_mean',
+        'computer_name': 'computer_name',
+        'behavior_session_uuid': 'behavior_session_uuid',
+    }
+
+    annotate.annotate_parameters(trials, metadata, keydict=keydict, inplace=True)
+    annotate.annotate_startdatetime(trials, metadata, inplace=True)
+    annotate.explode_startdatetime(trials, inplace=True)
+    annotate.annotate_n_rewards(trials, inplace=True)
+    #annotate.annotate_rig_id(trials, metadata, inplace=True) # MJD 2023
+    annotate.fix_autorearded(trials, inplace=True)
+    annotate.annotate_cumulative_reward(trials, metadata, inplace=True)
+    # annotate.annotate_filename(trials, filename, inplace=True)
+
+    for col in ('auto_rewarded', 'change_time', ):
+        if col not in trials.columns:
+            trials[col] = None
+
+    # add some columns that require calculation
+    annotate.make_trials_contiguous(trials, time, visual_stimuli, inplace=True)
+
+    trials['trial_type'] = annotate.categorize_trials(trials)
+    trials['lick_frames'] = annotate.get_lick_frames(trials, licks)
+
+  
+
+    annotate.update_times(trials, time, inplace=True)
+
+    # bouts MJD (must be after update_times, where lick_times are corrected)
+    trials["lick_bout_times"] = annotate.get_lick_bout_times(trials, licks)
+
+    annotate.annotate_lick_vigor(trials, licks, inplace=True)
+    annotate.calculate_latency(trials, inplace=True)
+    annotate.calculate_reward_rate(trials, inplace=True)
+
+    trials['response'] = annotate.check_responses(trials)
+    trials['color'] = trials.apply(annotate.assign_color, axis=1)
+    trials['response_type'] = annotate.get_response_type(trials)
+
+    try:
+        annotate.remove_repeated_licks(trials, inplace=True)
+    except Exception as e:
+        logger.warning("Could not remove repeated licks: {}".format(e))
+
+    if 'index' in trials.columns:
+        trials = trials.drop(columns=['index'])
+
+    return trials
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, datetime.date):
+            return obj.isoformat()
